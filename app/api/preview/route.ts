a2bb7e224c517136c717eace310017ed5b34b450
Juan Guillermo Murillo <jg.murillo10@uniandes.edu.co>
2024-04-15 23:03:42 -0500

    Comment PreviewBar

--- app/api/preview/route.ts
+++ app/api/preview/route.ts
@@ -1,58 +1,58 @@
 import { validatePreview, getDynamicPageURL } from "@agility/nextjs/node";
-import { draftMode } from 'next/headers'
+import { draftMode } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest, res: NextResponse) {
+  const searchParams = request.nextUrl.searchParams;
 
-	const searchParams = request.nextUrl.searchParams
+  const agilityPreviewKey = searchParams.get("agilitypreviewkey");
+  const slug = searchParams.get("slug");
+  const ContentID = searchParams.get("ContentID");
 
-	const agilityPreviewKey = searchParams.get('agilitypreviewkey')
-	const slug = searchParams.get('slug')
-	const ContentID = searchParams.get('ContentID')
+  //validate our preview key, also validate the requested page to preview exists
+  const validationResp = await validatePreview({
+    agilityPreviewKey,
+    slug,
+  });
 
+  if (validationResp.error) {
+    return new Response(`${validationResp.message}`, {
+      status: 401,
+    });
+  }
 
-	//validate our preview key, also validate the requested page to preview exists
-	const validationResp = await validatePreview({
-		agilityPreviewKey,
-		slug
-	});
+  let previewUrl = slug;
 
-	if (validationResp.error) {
-		return new Response(`${validationResp.message}`, {
-			status: 401
-		});
-	}
+  //TODO: these kinds of dynamic links should work by default (even outside of preview)
+  if (ContentID) {
+    const dynamicPath = await getDynamicPageURL({
+      contentID: ContentID,
+      preview: true,
+      slug,
+    });
+    if (dynamicPath) {
+      previewUrl = dynamicPath;
+    }
+  }
 
-	let previewUrl = slug;
+  //enable draft/preview mode
+  draftMode().enable();
 
-	//TODO: these kinds of dynamic links should work by default (even outside of preview)
-	if (ContentID) {
-		const dynamicPath = await getDynamicPageURL({ contentID: ContentID, preview: true, slug });
-		if (dynamicPath) {
-			previewUrl = dynamicPath;
-		}
-	}
+  // Redirect to the slug
+  //Add an extra querystring to the location header - since Netlify will keep the QS for the incoming request by default
+  let url = `${previewUrl}`;
+  if (url.includes("?")) {
+    url = `${url}&preview=1`;
+  } else {
+    url = `${url}?preview=1`;
+  }
 
-	//enable draft/preview mode
-	draftMode().enable()
+  return new Response(`Initializing preview mode`, {
+    status: 307,
+    headers: {
+      Location: url,
+    },
+  });
 
-	// Redirect to the slug
-	//Add an extra querystring to the location header - since Netlify will keep the QS for the incoming request by default
-	let url = `${previewUrl}`
-	if (url.includes("?")) {
-		url = `${url}&preview=1`
-	} else {
-		url = `${url}?preview=1`
-	}
-
-	return new Response(`Initializing preview mode`, {
-		status: 307,
-		headers: {
-			"Location": url,
-		}
-
-	});
-
-	NextResponse.redirect(url)
-
+  NextResponse.redirect(url);
 }
